0964ad358f12b01d09946bb321b836f5d2efcac1
Dalufishe <fishawe87@gmail.com>
2023-07-11 19:15:55 +0800

    feat: score count

--- pages/[gameid]/index.js
+++ pages/[gameid]/index.js
@@ -3,33 +3,85 @@
  * * "../../components/pages/GamePage"
  */
 
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import Viewer from "../../components/pages/GamePage/Viewer/Viewer";
 import nrrd from "../../constant/nrrd";
 import { cx } from "@emotion/css";
 import ConfirmBtn from "../../components/pages/GamePage/ConfirmBtn/ConfirmBtn";
 
 export default function Index() {
-  const idArray = useRef([0]);
+  // states
+  const [idArray, setIdArray] = useState([]);
   const [currentIdIndex, setCurrentIdIndex] = useState(0);
+  const [labels, setLabels] = useState([]);
+  const [select, setSelect] = useState([]);
+  const [isEndGame, setIsEndGame] = useState(false);
 
+  // get random nrrd ids
   useEffect(() => {
-    for (let i = 1; i < 10; i++) {
-      idArray.current[i] = Math.floor(Math.random() * nrrd.count);
+    const arr = [];
+    for (let i = 0; i < 10; i++) {
+      arr.push(Math.floor(Math.random() * nrrd.count));
     }
+    setIdArray(arr);
   }, []);
 
-  const handleNext = () => {
-    if (currentIdIndex >= idArray.current.length - 1)
-      setCurrentIdIndex(idArray.current.length - 1);
-    else {
-      setCurrentIdIndex(currentIdIndex + 1);
+  // get labels data
+  useEffect(() => {
+    fetch("./volume/meta.json")
+      .then((res) => res.json())
+      .then((json) => {
+        const { nrrd } = json;
+        const arr = [];
+        nrrd.map((n) => {
+          for (let id of idArray) {
+            if (id === Number(n.id)) {
+              arr.push(n.label);
+            }
+          }
+        });
+        setLabels(arr);
+        console.log(arr);
+      });
+  }, [idArray]);
+
+  // logics
+  const handleNext = useCallback(
+    (s) => {
+      setSelect([...select, s]);
+
+      if (currentIdIndex >= idArray.length - 1) {
+        if (currentIdIndex === idArray.length - 1) {
+          setIsEndGame(true);
+        }
+        setCurrentIdIndex(idArray.length - 1);
+      } else {
+        setCurrentIdIndex(currentIdIndex + 1);
+      }
+    },
+    [currentIdIndex, idArray]
+  );
+
+  const handleScore = useCallback(() => {
+    let score = 0;
+
+    for (let i = 0; i < labels.length; i++) {
+      // hand-write
+      if (labels[i] > 0.1) {
+        if (select[i]) score++;
+      }
+      // no hand-write
+      else {
+        if (!select[i]) score++;
+      }
     }
-  };
+
+    return score;
+  }, [labels, select]);
 
   return (
     <div>
-      <Viewer id={idArray.current[currentIdIndex]} />
+      {idArray.length ? <Viewer id={idArray[currentIdIndex]} /> : ""}
       {/* floating dom elements */}
       <div
         className={cx(
@@ -55,8 +107,28 @@ export default function Index() {
           "text-3xl"
         )}
       >
-        <ConfirmBtn onClick={handleNext}>No</ConfirmBtn>
-        <ConfirmBtn onClick={handleNext}>Yes</ConfirmBtn>
+        {isEndGame ? (
+          <>
+            <ConfirmBtn>Score - {handleScore()}</ConfirmBtn>
+          </>
+        ) : (
+          <>
+            <ConfirmBtn
+              onClick={() => {
+                handleNext(0);
+              }}
+            >
+              No
+            </ConfirmBtn>
+            <ConfirmBtn
+              onClick={() => {
+                handleNext(1);
+              }}
+            >
+              Yes
+            </ConfirmBtn>
+          </>
+        )}
       </div>
     </div>
   );
